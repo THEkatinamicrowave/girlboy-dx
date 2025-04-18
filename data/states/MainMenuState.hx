9e45c7f5c56d10978ca1a2b8cbf1d5efcc35eda1
KadePleaseHelpMe <cisforcookiemoonster@gmail.com>
2025-04-18 14:33:39 -0400

    goober

--- data/states/MainMenuState.hx
+++ data/states/MainMenuState.hx
@@ -4,17 +4,21 @@ var codestring:String = "";
 function postCreate() codestring = "";
 
 function postUpdate(elapsed:Float) {
-    if (FlxG.keys.justPressed.G) codestring += "G";
-    if (FlxG.keys.justPressed.I) codestring += "I";
+    trace(codestring);
     if (FlxG.keys.justPressed.R) codestring += "R";
+    if (FlxG.keys.justPressed.U) codestring += "U";
+    if (FlxG.keys.justPressed.N) codestring += "N";
+    if (FlxG.keys.justPressed.K) codestring += "K";
+    if (FlxG.keys.justPressed.I) codestring += "I";
     if (FlxG.keys.justPressed.L) codestring += "L";
-    if (FlxG.keys.justPressed.B) codestring += "B";
-    if (FlxG.keys.justPressed.O) codestring += "O";
-    if (FlxG.keys.justPressed.Y) codestring += "Y";
+    if (FlxG.keys.justPressed.P) codestring += "P";
+    if (FlxG.keys.justPressed.G) codestring += "G";
+    if (FlxG.keys.justPressed.M) codestring += "M";
+    if (FlxG.keys.justPressed.PERIOD) codestring += ".";
     if (FlxG.keys.justPressed.E) codestring += "E";
     if (FlxG.keys.justPressed.X) codestring += "X";
 
-	if (codestring.toLowerCase() == "girlboyexe") {
+    if (codestring.toLowerCase() == "runkillprgm.exe") {
 		persistentUpdate = false;
 		FlxG.sound.music.stop();
 
