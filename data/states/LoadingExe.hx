2e39ece5643089930f30395842be5c9468cd08aa
KadePleaseHelpMe <cisforcookiemoonster@gmail.com>
2025-04-18 11:14:02 -0400

    skibidi rizzler

--- data/states/LoadingExe.hx
+++ data/states/LoadingExe.hx
@@ -1,7 +1,56 @@
-//
-var bigfuckintext:FunkinText;
+// Main loading state for the Girlboy DX mod.
+
+var loadingText: FunkinText; // Text object for displaying loading messages.
 
 function create() {
-    bigfuckintext = new FunkinText(5, 5, FlxG.width - 10, "FUCK YOU.\nYOU FUCKIN DICK.", 10);
-    add(bigfuckintext);
+    // Initialize the loading text display.
+    loadingText = new FunkinText(5, 5, FlxG.width - 10, "Friday Night Funkin' Codename Engine: Girlboy.xml DX Mod.", 24);
+    add(loadingText);
+
+    // Sequentially display loading messages with delays.
+    displayLoadingSequence();
+}
+
+/**
+ * Displays the sequence of loading messages with delays.
+ */
+function displayLoadingSequence() {
+    addGoof("\nLoading assets...", 0, () -> {
+        addGoof("\n\nERROR: corrupted file detected.", 2, () -> {
+            addGoof("\nAttempting to fix...", 0.5, () -> {
+                addGoof("\n\nFAILURE: file could not be fixed.", 0.1, () -> {
+                    addGoof("\nAttempting workaround...", 2, () -> {
+                        addGoof("\nAwaiting permissions...", 5, () -> {
+                            addGoof("\n\nPermissions forged!", 9, () -> {
+                                addGoof("\n\n\nRunning...", 0.1, () -> {
+                                    addGoof("\n", 6, killText);
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+}
+
+function addGoof(text: String, wait: Float, doafter: Void -> Void) {
+    if (wait == 0) {
+        loadingText.text += text;
+        doafter();
+    } else {
+        new FlxTimer().start(wait, () -> {
+            loadingText.text += text;
+            doafter();
+        });
+    }
+}
+
+function killText() {
+    loadingText.text = "";
+
+    new FlxTimer().start(1, () -> {
+        PlayState.loadSong("corruption");
+        FlxG.switchState(new PlayState());
+    });
 }
--- data/states/MainMenuState.hx
+++ data/states/MainMenuState.hx
@@ -1,29 +1,23 @@
 //
-import funkin.backend.MusicBeatTransition;
-
 var codestring:String = "";
 
-function postCreate() {
-	codestring = "";
-}
+function postCreate() codestring = "";
 
 function postUpdate(elapsed:Float) {
-    var keyMap = ["G", "I", "R", "L", "B", "O", "Y", "E", "X"];
-    for (key in keyMap) {
-        if (FlxG.keys.justPressed[key]) {
-            codestring += key;
-        }
-    }
+    if (FlxG.keys.justPressed.G) codestring += "G";
+    if (FlxG.keys.justPressed.I) codestring += "I";
+    if (FlxG.keys.justPressed.R) codestring += "R";
+    if (FlxG.keys.justPressed.L) codestring += "L";
+    if (FlxG.keys.justPressed.B) codestring += "B";
+    if (FlxG.keys.justPressed.O) codestring += "O";
+    if (FlxG.keys.justPressed.Y) codestring += "Y";
+    if (FlxG.keys.justPressed.E) codestring += "E";
+    if (FlxG.keys.justPressed.X) codestring += "X";
 
 	if (codestring.toLowerCase() == "girlboyexe") {
 		persistentUpdate = false;
 		FlxG.sound.music.stop();
 
-		MusicBeatTransition.script = "data/scripts/broken";
 		FlxG.switchState(new ModState("LoadingExe"));
-	}
-
-	if (FlxG.keys.justPressed.ANY) {
-		trace(codestring);
 	}
 }
