0077c524e8d906fadc16584841a2970d683b3302
KadePleaseHelpMe <cisforcookiemoonster@gmail.com>
2025-04-19 21:49:46 -0400

    remove goofy debug trace

--- data/global.hx
+++ data/global.hx
@@ -2,7 +2,6 @@
 import funkin.menus.StoryMenuState;
 
 function preStateSwitch() {
-    trace(Std.isOfType(FlxG.game._requestedState, FreeplayState));
     if (Std.isOfType(FlxG.game._requestedState, FreeplayState))
         FlxG.game._requestedState = new StoryMenuState();
 }
