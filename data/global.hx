f9349ec14c503bbf8203e123cd37b634196f8045
KadePleaseHelpMe <cisforcookiemoonster@gmail.com>
2025-08-05 20:11:04 -0400

    some stuff (possibly for 1.1)

--- data/global.hx
+++
@@ -1,7 +0,0 @@
-//
-import funkin.menus.StoryMenuState;
-
-function preStateSwitch() {
-    if (Std.isOfType(FlxG.game._requestedState, FreeplayState))
-        FlxG.game._requestedState = new StoryMenuState();
-}
