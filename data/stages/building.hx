cf957c7245af10589be923d4cff95e4fbe5f6927
KadePleaseHelpMe <cisforcookiemoonster@gmail.com>
2025-08-17 18:40:08 -0500

    buildingstage shit (-26 lines)

--- data/stages/building.hx
+++ data/stages/building.hx
@@ -1,16 +1,9 @@
 //
 var curColor:Int = 0;
-var colors = [
-	0xFF31A2FD,
-	0xFF31FD8C,
-	0xFFFB33F5,
-	0xFFFD4531,
-	0xFFFBA633
-];
+var colors = [0xFF31A2FD, 0xFF31FD8C, 0xFFFB33F5, 0xFFFD4531, 0xFFFBA633];
 
 var p1shader:CustomShader;
 var p2shader:CustomShader;
-
 var bgBuildingsShader:CustomShader;
 var fgBuildingsShader:CustomShader;
 
@@ -18,101 +11,82 @@ var bigFuckinLight:FlxSprite;
 var otherFuckinLight:FlxSprite;
 
 function postCreate() {
-    if (stage.stageName == "building") {
-        p1shader = new CustomShader("bullshit");
-        p2shader = new CustomShader("bullshit");
-        bgBuildingsShader = new CustomShader("bullshit");
-        fgBuildingsShader = new CustomShader("bullshit");
+	if (stage.stageName != "building") return;
 
-        bigFuckinLight = new FlxSprite().loadGraphic(Paths.image("stages/building/dx/coollights"));
-        insert(members.indexOf(bg) + 1, bigFuckinLight);
+	p1shader = new CustomShader("bullshit");
+	p2shader = new CustomShader("bullshit");
+	bgBuildingsShader = new CustomShader("bullshit");
+	fgBuildingsShader = new CustomShader("bullshit");
 
-        otherFuckinLight = new FlxSprite().loadGraphic(Paths.image("stages/building/dx/coollights"));
-        insert(members.indexOf(bigFuckinLight), otherFuckinLight);
+	bigFuckinLight = new FlxSprite().loadGraphic(Paths.image("stages/building/dx/coollights"));
+	insert(members.indexOf(bg) + 1, bigFuckinLight);
 
-        for (light in [bigFuckinLight, otherFuckinLight]) {
-            light.updateHitbox();
-            light.alpha = 0;
-            light.blend = 0;
-            light.scrollFactor.set();
-            light.screenCenter();
-        }
+	otherFuckinLight = new FlxSprite().loadGraphic(Paths.image("stages/building/dx/coollights"));
+	insert(members.indexOf(bigFuckinLight), otherFuckinLight);
 
-        otherFuckinLight.visible = !bigFuckinLight.visible;
+	for (light in [bigFuckinLight, otherFuckinLight]) {
+		light.updateHitbox();
+		light.alpha = light.blend = 0;
+		light.scrollFactor.set();
+		light.screenCenter();
+	}
 
-        boyfriend.shader = p1shader;
-        dad.shader = p2shader;
-        buildings.shader = bgBuildingsShader;
-        roof.shader = fgBuildingsShader;
+	otherFuckinLight.visible = !bigFuckinLight.visible;
 
-        p1shader.maskPos = [3, 6];
-        p2shader.maskPos = [-3, 5];
-        bgBuildingsShader.maskPos = [0, 10];
-        fgBuildingsShader.maskPos = [0, 17];
+	boyfriend.shader = p1shader;
+	dad.shader = p2shader;
+	buildings.shader = bgBuildingsShader;
+	roof.shader = fgBuildingsShader;
 
-        for (shader in [p1shader, p2shader, bgBuildingsShader, fgBuildingsShader])
-            shader.mixValue = 0;
-    }
+	p1shader.maskPos = [3, 6];
+	p2shader.maskPos = [-3, 5];
+	bgBuildingsShader.maskPos = [0, 10];
+	fgBuildingsShader.maskPos = [0, 17];
+
+	for (shader in [p1shader, p2shader, bgBuildingsShader, fgBuildingsShader]) shader.mixValue = 0;
 }
 
 function postUpdate(elapsed:Float) {
-    bigFuckinLight.angle += elapsed*Conductor.stepCrochet / 3;
-    otherFuckinLight.angle -= elapsed*Conductor.stepCrochet / 3;
+	bigFuckinLight.angle += (Conductor.stepCrochet / 3) * elapsed;
+	otherFuckinLight.angle -= (Conductor.stepCrochet / 3) * elapsed;
 
-    for (light in [bigFuckinLight, otherFuckinLight])
-        light.scale.set(Math.sqrt(2) / camGame.zoom, Math.sqrt(2) / camGame.zoom);
+	for (light in [bigFuckinLight, otherFuckinLight])
+		light.scale.set(Math.sqrt(2) / camGame.zoom, Math.sqrt(2) / camGame.zoom); // diagonal square
 }
 
 function beatHit(beat:Int) {
-    if (beat % camZoomingInterval == 0) {
-        curColor = (curColor + FlxG.random.int(1, colors.length - 1)) % colors.length;
+	if (beat % camZoomingInterval != 0) return;
 
-        var newColor = colors[curColor];
-        var glowColor = [
-            ((newColor >> 16) & 0xFF) / 255, 
-            ((newColor >> 8) & 0xFF) / 255, 
-            (newColor & 0xFF) / 255
-        ];
+	curColor = (curColor + FlxG.random.int(1, colors.length - 1)) % colors.length;
 
-        for (meat in [bigFuckinLight, otherFuckinLight]) {
-            meat.visible = !meat.visible;
-            meat.color = newColor;
-        }
+	var newColor = colors[curColor];
+	var glowColor = [
+		((newColor >> 16) & 0xFF) / 255,
+		((newColor >> 8) & 0xFF) / 255,
+		(newColor & 0xFF) / 255
+	];
 
-        rooflights.color = newColor;
+	for (meat in [bigFuckinLight, otherFuckinLight]) {
+		meat.visible = !meat.visible;
+		meat.color = newColor;
+	}
 
-        for (shader in [p1shader, p2shader, bgBuildingsShader, fgBuildingsShader]) {
-            shader.glowColor = glowColor;
-        }
-    }
+	rooflights.color = newColor;
+	for (shader in [p1shader, p2shader, bgBuildingsShader, fgBuildingsShader]) shader.glowColor = glowColor;
 }
 
-function onEvent(event:EventGameEvent) {
-    if (event.event.name == "Start Buildings Goober Event") {
-        var toggle:Bool = event.event.params[0];
+function onEvent(_e:EventGameEvent) {
+	var e = _e.event;
+	if (e.name != "Start Buildings Goober Event") return;
 
-        camZooming = true;
+	var toggle:Bool = e.params[0];
 
-        if (toggle) {
-            bigFuckinLight.alpha = 0.6;
-            otherFuckinLight.alpha = 0.45;
-            rooflights.alpha = 1;
+	camZooming = true;
 
-            for (shader in [p1shader, p2shader, bgBuildingsShader, fgBuildingsShader]) {
-                shader.mixValue = 1;
-            }
+	rooflights.alpha = toggle ? 1 : 0;
+	bigFuckinLight.alpha = rooflights.alpha * 0.6;
+	otherFuckinLight.alpha = rooflights.alpha * 0.45;
+	defaultCamZoom = toggle ? 0.45 : stage.stageXML.get("zoom");
 
-            PlayState.instance.defaultCamZoom = 0.45;
-        } else {
-            bigFuckinLight.alpha = 0;
-            otherFuckinLight.alpha = 0;
-            rooflights.alpha = 0;
-
-            for (shader in [p1shader, p2shader, bgBuildingsShader, fgBuildingsShader]) {
-                shader.mixValue = 0;
-            }
-
-            PlayState.instance.defaultCamZoom = stage.stageXML.get("zoom");
-        }
-    }
+	for (shader in [p1shader, p2shader, bgBuildingsShader, fgBuildingsShader]) shader.mixValue = rooflights.alpha;
 }
