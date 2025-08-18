cf870f780816df91d47e0d69bd32476ae1f98c6a
KadePleaseHelpMe <cisforcookiemoonster@gmail.com>
2025-08-18 12:46:11 -0500

    corruption stage event shit

--- data/stages/corruption.hx
+++ data/stages/corruption.hx
@@ -1,12 +1,14 @@
 //
+import flixel.FlxObject;
 import Type;
 
 var roomsCentrePoint:FlxPoint;
 
 var moveSpeed:Float = 0.5,
+	lerping:Bool = true,
 	sideSeparation:Float = 182;
 
-var psmm:Map<FlxSprite, Array<{sprite:FlxSprite, relativePos:FlxPoint}>>; // proportionalSpriteMovementMap
+var psmm:Map<FlxObject, Array<{sprite:FlxSprite, relativePos:FlxPoint}>>; // proportionalSpriteMovementMap
 
 function postCreate() {
 	roomsCentrePoint = FlxPoint.get(
@@ -27,14 +29,49 @@ function postUpdate(elapsed:Float) {
 	var targetRoomL = roomsCentrePoint.x - halfSeparation - roomL.width;
 	var targetRoomR = roomsCentrePoint.x + halfSeparation;
 
-	roomL.x = CoolUtil.fpsLerp(roomL.x, targetRoomL, 0.16);
-	roomR.x = CoolUtil.fpsLerp(roomR.x, targetRoomR, 0.16);
+	trace("halfSeparation", halfSeparation);
+	trace("targetRoomL", targetRoomL);
+	trace("targetRoomR", targetRoomR);
 
-	for (s in psmm.keys()) {
-		for (entry in psmm[s]) {
-			var isCharacter:Bool = Type.getClassName(Type.getClass(s)) == "funkin.game.Character";
+	roomL.x = CoolUtil.fpsLerp(roomL.x, targetRoomL, lerping ? 0.16 : 1);
+	roomR.x = CoolUtil.fpsLerp(roomR.x, targetRoomR, lerping ? 0.16 : 1);
+
+	trace("roomL.x", roomL.x);
+	trace("roomR.x", roomR.x);
+	
+	for (s=>arr in psmm) {
+		for (entry in arr) {
+			var isCharacter = Type.getClass(s) == Character;
 			s.x = CoolUtil.fpsLerp(s.x, entry.relativePos.x + entry.sprite.x, isCharacter ? 1 : 0.06);
 			s.y = CoolUtil.fpsLerp(s.y, entry.relativePos.y + entry.sprite.y, isCharacter ? 1 : 0.06);
 		}
+	}
+}
+
+function onEvent(_e:EventGameEvent) {
+	var e = _e.event;
+	if (e.name != "Corruption Stage Gap Size") return;
+	
+	var p = e.params;
+	var s = p[0];
+	var tt = p[1] == 0 ? 0.001 : p[1];
+
+	FlxTween.cancelTweensOf(sideSeparation);
+	
+	var isTween = p[2] != "CLASSIC";
+	if (isTween) {
+		lerping = false;
+		var initialVal = sideSeparation;
+		var te = CoolUtil.flxeaseFromString(p[2], p[3]);
+
+		FlxTween.num(initialVal, s, (Conductor.stepCrochet/1000) * tt, { ease: te, onComplete: ()->{
+			lerping = true;
+		}}, (v:Float)->{
+			sideSeparation = v;
+			trace("sideSeparation", sideSeparation);
+		});
+	} else {
+		lerping = true;
+		sideSeparation = e.params[0];
 	}
 }
