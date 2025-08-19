a0a90fc00903d1e909a4be24a0e78969af00b5ab
KadePleaseHelpMe <cisforcookiemoonster@gmail.com>
2025-08-19 12:00:56 -0500

    more keys can be pressed in mainmenustate

--- data/states/MainMenuState.hx
+++ data/states/MainMenuState.hx
@@ -1,24 +1,70 @@
 //
-var codestring:String = "";
+import flixel.input.keyboard.FlxKey;
+
+var codeString:String = "";
 
 function postUpdate(elapsed:Float) {
-	if (FlxG.keys.justPressed.R) codestring += "R";
-	if (FlxG.keys.justPressed.U) codestring += "U";
-	if (FlxG.keys.justPressed.N) codestring += "N";
-	if (FlxG.keys.justPressed.K) codestring += "K";
-	if (FlxG.keys.justPressed.I) codestring += "I";
-	if (FlxG.keys.justPressed.L) codestring += "L";
-	if (FlxG.keys.justPressed.P) codestring += "P";
-	if (FlxG.keys.justPressed.G) codestring += "G";
-	if (FlxG.keys.justPressed.M) codestring += "M";
-	if (FlxG.keys.justPressed.PERIOD) codestring += ".";
-	if (FlxG.keys.justPressed.E) codestring += "E";
-	if (FlxG.keys.justPressed.X) codestring += "X";
+    if (FlxG.keys.firstJustPressed() != -1) {
+        var kInt = FlxG.keys.firstJustPressed();
+        var k = FlxKey.toStringMap.get(kInt);
+        var extras = [
+            "A" => "A",
+            "B" => "B",
+            "C" => "C",
+            "D" => "D",
+            "E" => "E",
+            "F" => "F",
+            "G" => "G",
+            "H" => "H",
+            "I" => "I",
+            "J" => "J",
+            "K" => "K",
+            "L" => "L",
+            "M" => "M",
+            "N" => "N",
+            "O" => "O",
+            "P" => "P",
+            "Q" => "Q",
+            "R" => "R",
+            "S" => "S",
+            "T" => "T",
+            "U" => "U",
+            "V" => "V",
+            "W" => "W",
+            "X" => "X",
+            "Y" => "Y",
+            "Z" => "Z",
 
-	if (codestring.toLowerCase() == "runkillprgm.exe") {
+            "ZERO" => "0",
+            "ONE" => "1",
+            "TWO" => "2",
+            "THREE" => "3",
+            "FOUR" => "4",
+            "FIVE" => "5",
+            "SIX" => "6",
+            "SEVEN" => "7",
+            "EIGHT" => "8",
+            "NINE" => "9",
+
+            "PERIOD" => ".",
+            "COMMA" => ",",
+            "SEMICOLON" => ";",
+            "QUOTE" => "'",
+            "SLASH" => "/",
+            "MINUS" => "-"
+        ];
+        if (extras.exists(k)) {
+            k = extras.get(k);
+        
+            codeString += k;
+            trace(codeString);
+        }
+    }
+
+    if (codeString.toLowerCase() == "runkillprgm.exe") {
 		persistentUpdate = false;
 		FlxG.sound.music.stop();
 
 		FlxG.switchState(new ModState("LoadingExe"));
-	}
+    }
 }
