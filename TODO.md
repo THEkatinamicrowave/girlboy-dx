bd6b002d95f641fdef84c45e5ef982cfafeed50b
KadePleaseHelpMe <cisforcookiemoonster@gmail.com>
2025-08-19 16:55:33 -0500

    LoadingState isn't cancerous to look at anymore

--- TODO.md
+++ TODO.md
@@ -1,4 +1,3 @@
 TODO BEFORE V1.1 RELEASE:
 
-- Fix LoadingState being SHIT to code in
 - Make loadingstate look better and not be FUCKING WHITE TEXT ON BLACK
--- data/states/LoadingExe.hx
+++ data/states/LoadingExe.hx
@@ -1,51 +1,45 @@
 //
-var loadingText:FunkinText;
+
+var textShit:FunkinText;
+var curLine:Int = 0;
+var defaultHeader:String = "Friday Night Funkin' Codename Engine: Girlboy.xml DX Mod.\n\n";
+
+var lines:Array<{t:String, w:Float, c:Bool}> = [ // text, waittime, clear
+	{t: "Loading assets...", w: 2, c: false},
+	{t: "\n\nERROR: corrupted file detected.", w: 0.5, c: false},
+	{t: "\nAttempting to fix...", w: 0.1, c: false},
+	{t: "\n\nFAILURE: file could not be fixed.", w: 2, c: false},
+	{t: "\nAttempting workaround...", w: 5, c: false},
+	{t: "\nAwaiting permissions...", w: 9, c: false},
+	{t: "\n\nPermission denied.\nForging permissions...", w: 4, c: false},
+	{t: "\nPermissions forged!", w: 2.3, c: false},
+	{t: "Running...", w: 5, c: true}
+];
 
 function create() {
-	loadingText = new FunkinText(5, 5, FlxG.width - 10, "Friday Night Funkin' Codename Engine: Girlboy.xml DX Mod.", 24);
-	add(loadingText);
-
-	displayLoadingSequence();
+	textShit = new FunkinText(5, 5, FlxG.width - 10, defaultHeader, 24);
+	add(textShit);
+	runTextLines();
 }
 
-function displayLoadingSequence() {
-	addGoof("\nLoading assets...", 0, () -> {
-		addGoof("\n\nERROR: corrupted file detected.", 2, () -> {
-			addGoof("\nAttempting to fix...", 0.5, () -> {
-				addGoof("\n\nFAILURE: file could not be fixed.", 0.1, () -> {
-					addGoof("\nAttempting workaround...", 2, () -> {
-						addGoof("\nAwaiting permissions...", 5, () -> {
-							addGoof("\n\nPermission denied.\nForging permissions...", 9, () -> {
-								addGoof("\nPermissions forged!", 4, () -> {
-									addGoof("\n\nRunning...", 2.3, () -> {
-										addGoof("\n", 3, killText);
-									});
-								});
-							});
-						});
-					});
-				});
-			});
-		});
+function runTextLines() {
+	if (curLine >= lines.length) {
+		killText();
+		return;
+	}
+	var l = lines[curLine];
+	if (l.c) textShit.text = defaultHeader;
+	textShit.text += l.t;
+
+	new FlxTimer().start(l.w, ()->{
+		curLine++;
+		runTextLines();
 	});
 }
 
-function addGoof(text:String, wait:Float, doafter:Void -> Void) {
-	if (wait == 0) {
-		loadingText.text += text;
-		doafter();
-	} else {
-		new FlxTimer().start(wait, () -> {
-			loadingText.text += text;
-			doafter();
-		});
-	}
-}
-
 function killText() {
-	loadingText.text = "";
-
-	new FlxTimer().start(1, () -> {
+	textShit.text = "";
+	new FlxTimer().start(1, ()->{
 		PlayState.loadSong("corruption");
 		FlxG.switchState(new PlayState());
 	});
