401ada110f9ab5a0c751e286a2446da9d2eb6cbb
KadePleaseHelpMe <cisforcookiemoonster@gmail.com>
2026-09-24 23:06:09 -0400

    believe it or not, this script took me 15 minutes to make
    it was really stupid

---
+++ songs/weaponry/scripts/not worth making this an event.hx
@@ -0,0 +1,14 @@
+// it is so not worth making a separate event for this thing it only happens once
+//
+function beatHit(beat:Int) {
+	switch (beat) {
+		case 256:
+			uiBadApple(true);
+		case 258:
+			uiBadApple(false);
+	}
+}
+
+function uiBadApple(doit:Bool) {
+    camHUD.color = doit ? 0xFF000000 : 0xFFFFFFFF;
+}
