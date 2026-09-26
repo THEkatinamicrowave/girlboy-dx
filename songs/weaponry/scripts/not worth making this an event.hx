1f7614faeace3fcaea8a9657bc1cad057fc3b240
KadePleaseHelpMe <cisforcookiemoonster@gmail.com>
2026-09-26 14:55:11 -0400

    better

--- songs/weaponry/scripts/not worth making this an event.hx
+++ songs/weaponry/scripts/not worth making this an event.hx
@@ -1,16 +1,19 @@
 // it is so not worth making a separate event for this thing it only happens once
 //
-function beatHit(beat:Int) {
-	if (PlayState.difficulty.toLowerCase() == 'dx') {
-		switch (beat) {
-			case 256:
-				uiBadApple(true);
-			case 258:
-				uiBadApple(false);
-		}
-	}
+import funkin.backend.utils.FlxInterpolateColor;
+var ci:FlxInterpolateColor;
+
+function create() ci = new FlxInterpolateColor(0xFFFFFF);
+
+function onEvent(_e:EventGameEvent) {
+	var e = _e.event;
+	if (PlayState.difficulty.toLowerCase() != "dx" || _e.cancelled || e.name != "Bad Apple") return;
+
+	uiBadApple(e.params[3]);
 }
 
-function uiBadApple(doit:Bool) {
-	camHUD.color = doit ? 0xFF000000 : 0xFFFFFFFF;
+function uiBadApple(mix:Float) {
+	ci.color = 0xFFFFFF;
+	ci.lerpTo(0xFFFFFF, mix);
+	camHUD.color = ci.color;
 }
